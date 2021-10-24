b773b1363d845a169d4f40f2d8a98ee9a6250b18
Shea Cronin <shealcronin@gmail.com>
2021-10-24 18:53:48 -0400

    Add description and image to home page

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
+import pokemart from './img/pokemart.png';
+
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <p>
+                Welcome to the Pokémart, where you can purchase items to assist
+                you on your adventure through the Pokémon world!
+            </p>
+            <img src={pokemart} alt="Pokémart icon" id="pokemart"></img>
         </div>
     );
 };
