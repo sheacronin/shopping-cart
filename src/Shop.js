6b8029cc8d02373748f75ba0ef47943309fbbe53
Shea Cronin <shealcronin@gmail.com>
2021-10-25 08:20:42 -0400

    Add basic styles of cart preview

--- src/Shop.js
+++ src/Shop.js
@@ -1,7 +1,20 @@
+import bag from './img/bag.png';
+import './Shop.css';
+
 const Shop = () => {
     return (
-        <div>
+        <div id="shop">
             <h1>Shop</h1>
+            <CartPreview />
+        </div>
+    );
+};
+
+const CartPreview = () => {
+    return (
+        <div id="cart-preview">
+            <img src={bag} alt="A bag" />
+            <div id="num-of-items">1</div>
         </div>
     );
 };
