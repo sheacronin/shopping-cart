6496a8a18358c3635c7f37b445708fb27b2f9ea8
Shea Cronin <shealcronin@gmail.com>
2021-10-26 19:19:15 -0400

    Add item quantity state to ItemCard

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import '../styles/ItemCard.css';
 
 const ItemCard = (props) => {
-    const { url, numOfThisItemInCart, handleAddToCart } = props;
+    const { url, handleAddToCart } = props;
     const [isReadyToRender, setIsReadyToRender] = useState(false);
 
     useEffect(() => {
@@ -31,27 +31,60 @@ const ItemCard = (props) => {
         return capitalizedWords.join(' ');
     };
 
+    const [itemQuantity, setItemQuantity] = useState(0);
+    const handleQuantityChange = (e) => {
+        const quantity = e.target.value;
+        if (isNaN(quantity)) {
+            return;
+        }
+
+        setItemQuantity(quantity);
+    };
+
     if (isReadyToRender) {
         return (
             <article className="item-card">
                 <h3>{formatName(item.name)}</h3>
                 <img src={item.sprites.default} alt={formatName(item.name)} />
                 <div>
-                    <button className="increment">-</button>
-                    <input className="quantity-input"></input>
-                    <button className="decrement">+</button>
-                    <button
-                        className="add-to-cart"
-                        onClick={() => handleAddToCart(item)}
-                    >
-                        Add to Cart
-                    </button>
+                    <DecrementButton />
+                    <input
+                        className="quantity-input"
+                        value={itemQuantity}
+                        onChange={handleQuantityChange}
+                    />
+                    <IncrementButton />
+                    <AddToCart
+                        item={item}
+                        quantity={itemQuantity}
+                        handleAddToCart={handleAddToCart}
+                    />
                 </div>
             </article>
         );
     } else {
         return null;
     }
+};
+
+const DecrementButton = () => {
+    return <button className="decrement">-</button>;
+};
+
+const IncrementButton = () => {
+    return <button className="increment">+</button>;
+};
+
+const AddToCart = (props) => {
+    const { item, quantity, handleAddToCart } = props;
+    return (
+        <button
+            className="add-to-cart"
+            onClick={() => handleAddToCart(item, quantity)}
+        >
+            Add to Cart
+        </button>
+    );
 };
 
 export default ItemCard;
